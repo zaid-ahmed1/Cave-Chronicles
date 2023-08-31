de7e31810aa456af47894cf1b38194b31346776a
bigz88 <zaid.ahmed@ucalgary.ca>
2023-08-30 19:29:46 -0600

    Camera follow and background bug fix

--- Assets/Scripts/Parallax.cs
+++ Assets/Scripts/Parallax.cs
@@ -1,30 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 using UnityEngine;
+using Vector3 = UnityEngine.Vector3;
 
 public class Parallax : MonoBehaviour
 {
-    private float length, startPos;
-    [SerializeField] new GameObject camera;
-    [SerializeField] float parallax;
+    [SerializeField] float parallaxMultiplier;
+    private Transform cameraTransform;
+    private Vector3 lastCameraPosition;
+    private float textureUnitSizeX;
 
-    void Start()
-    {
-        startPos = transform.position.x;
-        length = GetComponent<SpriteRenderer>().bounds.size.x;
+    private void Start() {
+        cameraTransform = Camera.main.transform;
+        lastCameraPosition = cameraTransform.position;
+        Sprite sprite = GetComponent<SpriteRenderer>().sprite;
+        Texture2D texture = sprite.texture;
+        textureUnitSizeX = texture.width / sprite.pixelsPerUnit;
     }
 
-    void Update()
-    {
-        float temp = (camera.transform.position.x * (1 - parallax));
-        float distance = (camera.transform.position.x * parallax);
-        transform.position = new Vector3(startPos + distance, transform.position.y, transform.position.z);
-
-        if (temp > startPos + length) {
-            startPos += length;
-        }
-        else if (temp < startPos - length) {
-            startPos-= length;
+    private void LateUpdate() {
+        Vector3 deltaMovement = cameraTransform.position - lastCameraPosition;
+        transform.position += deltaMovement * parallaxMultiplier;
+        lastCameraPosition = cameraTransform.position;
+        if (Mathf.Abs(cameraTransform.position.x - transform.position.x) >= textureUnitSizeX*3) {
+            float offsetPosition = (cameraTransform.position.x - transform.position.x) % textureUnitSizeX;
+            transform.position = new Vector3(cameraTransform.position.x + offsetPosition, transform.position.y);
         }
     }
+
 }
+
